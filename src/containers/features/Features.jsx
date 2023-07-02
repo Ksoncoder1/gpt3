561bb513d9267a8db714e4646204952834d3cbbb
Erickson Kimathi <ericksonkimathi@Ericksons-MacBook-Pro.local>
2023-07-02 18:33:28 +0300

    edited h1 tag in features component

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -26,7 +26,7 @@ const Features = () => {
   return (
     <div className='gpt3__features section__padding' id='features'>
       <div className='gpt3__features-heading'>
-        <h1 className='gradient__text'>The Future is Now and You Just Need to Realize It. Step Into The Future Today & Make it Happen</h1>
+        <h1 className='gradient__text'>The Future is Here, You Just Need to Realize It. Step Into The Future Today & Make it Happen</h1>
         <p>Request Early Access to Get Started</p>
       </div>
       <div className='gpt3__features-container'>
